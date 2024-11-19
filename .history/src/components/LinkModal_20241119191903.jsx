711f1c3c7e03b57de970b1c984d8cabdffe26d69
Wuis_01 <lordwuiserous@gmail.com>
2024-11-19 21:31:10 +0530

    checking if [prpgress bar works

--- .history/src/components/LinkModal_20241119191903.jsx
+++ .history/src/components/LinkModal_20241119191903.jsx
@@ -22,7 +22,7 @@ export default function LinkModal(props) {
     if (props.children == 0) return (
         <div className="fixed w-full h-full inset-0 bg-blue-transparent z-10" id="wrapper"
         onClick={hide}>
-            <div className={`fixed top-60 left-[480px] w-[200px] p-2 h-fit backdrop-blur-sm bg-white/5 transform transition-all duration-100 text-black overflow-auto rounded ${isVisible ? "scale-100 opacity-100" : "scale-50 opacity-0"}`}>
+            <div className={`fixed top-60 left-[480px] w-[100px] p-2 h-fit backdrop-blur-sm bg-white/5 transform transition-all duration-100 text-black overflow-auto rounded ${isVisible ? "scale-100 opacity-100" : "scale-50 opacity-0"}`}>
             <h4 className="text-white">Link to: </h4>
             <div className="flex justify-center items-center">
                 <p className="text-white">No tasks added yet.</p>
@@ -34,7 +34,7 @@ export default function LinkModal(props) {
     return (
         <div className="fixed w-full h-full inset-0 bg-blue-transparent z-10" id="wrapper"
         onClick={hide}>
-            <div className={`fixed top-60 left-[480px] w-[153px] p-2 h-fit backdrop-blur-sm bg-white/5 transform transition-all duration-100 text-black overflow-auto rounded ${isVisible ? "scale-100 opacity-100" : "scale-50 opacity-0"}`}>
+            <div className={`fixed top-60 left-[480px] w-[200px] p-2 h-fit backdrop-blur-sm bg-white/5 transform transition-all duration-100 text-black overflow-auto rounded ${isVisible ? "scale-100 opacity-100" : "scale-50 opacity-0"}`}>
             <h4 className="text-white">Link to: </h4>
             <div className="flex flex-wrap gap-2">
                 {props.children}
