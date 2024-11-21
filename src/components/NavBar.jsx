316adbe0490773e6621c036a9c7ee7120dba00e3
Anil Pal <anilcodes01@gmail.com>
2024-11-21 12:53:43 +0530

    give the name to app

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ export default function NavBar(props) {
     return (
       <div className={`rounded-xl col-span-4 ${props.bgColor} flex items-center justify-end p-2`}>
         {props.Button}
+       
       </div>
     )
 }
